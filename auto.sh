104374ed768bbb169cbb2d3816f613fb00bbe404
Shabeeb M <74951749+itsmeshabeeb@users.noreply.github.com>
2022-09-17 06:01:37 +0530

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -5,5 +5,5 @@ python3 manage.py makemigrations
 python3 manage.py migrate 
 python3 manage.py collectstatic --noinput
 #python3 manage.py collectstatic
-sudo systemctl restart nginx.service
-sudo systemctl restart gunicorn
+sudo systemctl restart apache2
+#sudo systemctl restart gunicorn
